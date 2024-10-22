8cca208fb8eddc9122ce03f5a42fc27ead2d3b25
Phfish <philip.skudvig@icloud.com>
2024-10-23 00:41:42 +0200

    snake go ╰(*°▽°*)╯

--- snake/constants.h
+++ snake/constants.h
@@ -1,15 +1,3 @@
 #define SCREEN_SIZE 25
 #define SCREEN_HALF SCREEN_SIZE/2
 #define TICK_SPEED 300000
-
-  // RIGHT == 261
-  // LEFT = 260
-  // UP == 259
-  // DOWN = 258
-
-// enum arrowDirection {
-//   DOWN = 258,
-//   UP = 259,
-//   LEFT = 260,
-//   RIGHT = 261,
-// };
